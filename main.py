dcc244cc62e7524645f74013c4444472255a89c8
Agata Dobrzyniewicz <s24305@pjwstk.edu.pl>
2023-12-08 18:18:15 +0100

    Init commit - main file and dataset added
    main file with main points of a project and dataset added

---
+++ main.py
@@ -0,0 +1,37 @@
+# AUTHORS:
+# Kurshakova Mariia
+# Dobrzyniewicz Agata
+# DATASET:
+# Kaggle.com - Hotel Booking Cancellation Prediction by YOUSSEF ABOELWAFA
+
+# imports
+
+
+# -- DATA IMPORT --
+
+# -- ANALYZE --
+# stats (min, max, mean, median, std, var, range)
+
+# look for missing or anomalies data
+
+# clear and normalise data
+
+# find correlations
+
+# -- VISUALISE --
+
+# -- SPLIT DATA --
+# train and test (do we need validation?)
+
+# -- MODEL CREATION --
+
+# -- PREDICTIONS --
+
+# -- ANALYZE PREDICTIONS --
+# calculate useful metrics
+
+# visualise important observations
+
+# -- SAVING PREDICTIONS --
+
+# -- SUMMARIZATION --
