74202a53ffc964b32d79900a1b276d02c3e046c9
MKurshakova <mariia.kurshakova@gmail.com>
2024-01-09 10:06:04 +0100

    data analysis

--- main.py
+++ main.py
@@ -11,12 +11,41 @@ import pandas as pd
 data = pd.read_csv("booking.csv")
 
 print(data.head())
-print(data.describe())
 print(data.size)
 print(data.columns)
 
-# -- ANALYZE -- - M
+# -- ANALYZE -- 
 # stats (min, max, mean, median, std, var, range)
+
+data = data.iloc[:, 1:]
+data.info()
+
+print("-------Count null values--------")
+print(data.isnull().sum().sort_values(ascending=False))
+print("-------Count uniq values--------")
+print(data.nunique())
+
+not_num_columns = ['type of meal', 'car parking space', 'room type',
+                   'market segment type', 'repeated', 'booking status']
+
+for column in not_num_columns:
+    print("----------")
+    print(data[column].value_counts())
+
+num_columns = ['number of adults', 'number of children', 'number of weekend nights',
+               'number of week nights', 'lead time', 'P-C', 'P-not-C',
+               'average price', 'special requests']
+for column in num_columns:
+    print("----------")
+    print("\nVALUE:", column)
+    print("MIN:", data[column].min())
+    print("MAX:", data[column].max())
+    print("MEAN:", data[column].mean())
+    print("MEDIAN:", data[column].median())
+    print("RANGE:", data[column].max() - data[column].min())
+    print("STANDARD DEVIATION:", data[column].std())
+    print("VARIANCE:", data[column].var())
+    print("PERCENTILE 90%:", data[column].quantile(0.9))
 
 # look for missing or anomalies data
 
